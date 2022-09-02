516597e1fb8481431c76e749e4e8e783cbc77a7d
VictorHOP <victorhugo.op@outlook.com>
2022-09-02 18:02:57 -0300

    finalizada a pagina quem somos e quase finalizada a produtos e servicos

---
+++ page-produtos-e-servicos.php
@@ -0,0 +1,170 @@
+<?php get_header(); ?>
+
+
+<div class="position-relative banner-pagina mt-5 container-fluid p-0">
+    <img src="<?php bloginfo('template_url'); ?>/assets/images/produtos-e-servicos-banner.png" alt="banner trem">
+    <h1 class="text-uppercase text-white position-h1"><?php the_title(); ?></h1>
+</div>
+
+<section class="container mrcla-te-ajuda">
+    <div class="row my-5">
+
+        <div class="d-flex flex-column col-6">
+            <h2 class="fw300 pb-4 azul-escuro">O QUE <fw900> FAZEMOS </fw900>
+            </h2>
+            <p class="cinza fw400">A Mitsui Rail Capital Latin America (MRCLA) oferece soluções integradas e personalizadas atendendo sua necessidade de mobilidade logística ferroviária. Somos referência em locação de locomotivas, vagões e terminais de cargas. <br><br>
+
+                Nossa expertise em materiais rodantes engloba as melhores soluções com a capacidade de atender com mais eficiência, segurança e confiabilidade nos diversos segmentos e setores. O transporte ferroviário preza pelo desenvolvimento sustentável na mobilidade de cargas.
+            </p>
+            <img src="<?php bloginfo('template_url'); ?>/assets/images/o-que-fazemos-trem1.png" alt="">
+        </div>
+        <div class="d-flex flex-column col-6">
+            <img src="<?php bloginfo('template_url'); ?>/assets/images/o-que-fazemos-trem2.png" alt="">
+
+            <p class="cinza fw400 pt-4">Temos know-how em soluções integradas e personalizadas. Contribuímos com seu negócio! Analisamos sua necessidade, avaliamos junto aos nossos fornecedores o material rodante mais adequado ao seu produto e trazemos uma solução financeira que seja favorável. <br><br>
+
+                Conte com a MRCLA para locação de vagões, locomotivas e terminais de cargas. <br><br>
+
+                Nosso atendimento é voltado a diversos setores, donos de cargas, operadores ferroviários e de terminais de cargas.
+
+            </p>
+        </div>
+    </div>
+</section>
+<section class=" bg-azul-roxo">
+    <div class="container">
+
+        <div class="row text-white py-6 justify-content-between flex-column flex-md-row">
+
+            <div class="col-11 col-md-5 d-flex align-items-center">
+                <h2 class="fw300 m-0">A MRCLA TE AJUDA
+                    A POTENCIALIZAR <fw900> SEU PODER DE ESCOLHA! </fw900>
+                </h2>
+            </div>
+
+            <div class="col-10 col-md-6">
+                <ul class="lista-produtos ps-0 ps-md-4 pt-5 pt-md-0">
+                    <li>
+                        <p>Alocar melhor seus recursos</p>
+                    </li>
+                    <li>
+                        <p>Prazos de pagamento flexíveis</p>
+                    </li>
+                    <li>
+                        <p>Aumentar a capacidade de transporte</p>
+                    </li>
+                    <li>
+                        <p>Maior segurança no transporte de cargas</p>
+                    </li>
+                    <li>
+                        <p>Melhorar a eficiência com ativos novos e modernos</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="section-servicos py-8">
+    <div class="container">
+        <div class="w-100 quebra-cabeca position-relative">
+            <img class="d-xl-block d-none" src="<?php bloginfo('template_url'); ?>/assets/images/fundo-quebra-cabeca.png" alt="">
+
+            <div class="row">
+                <div class="d-flex justify-content-between px-5 position-quebra-cabeca-cima flex-xl-row flex-column">
+                    <div class="d-flex flex-column col-12 col-xl-5 p-4">
+                        <h4 class="azul-escuro fs-24 ">Serviço Personalizado</h4>
+                        <p class="cinza">A MRCLA atende às necessidades e demandas dos clientes, oferecendo a melhor gestão de compra logística com soluções integradas de mobilidade para seu negócio.</p>
+                    </div>
+                    <div class="d-flex flex-column col-12 col-xl-5 p-4">
+                        <h4 class="azul-escuro fs-24">Vantagem competitiva</h4>
+                        <p class="cinza">Com a locação em ativos de mobilidade, é possível direcionar melhor os recursos do seu negócio. A responsabilidade de compra dos ativos rodantes fica por conta da MRCLA e sua empresa pode focar em sua atividade principal.</p>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between px-5 position-quebra-cabeca-baixo flex-xl-row flex-column">
+                    <div class="d-flex flex-column col-12 col-xl-6 p-4">
+                        <h4 class="azul-escuro fs-24 ">Qualidade e Segurança</h4>
+                        <p class="cinza">Garantia de qualidade e eficiência nos transportes de cargas. A MRCLA assegura a entrega dos vagões e locomotivas conforme as normas legais e de segurança. Dessa forma, você tem tranquilidade em todo o processo e adquire produtos novos que atendam às suas necessidades.</p>
+                    </div>
+                    <div class="d-flex flex-column col-12 col-xl-5 p-4">
+                        <h4 class="azul-escuro fs-24">Fluxo de caixa otimizado</h4>
+                        <p class="cinza">A MRCLA realiza a gestão da compra dos ativos de mobilidade adequados para o seu negócio e faz a locação à sua empresa. Você fica responsável apenas pela contratação do seguro e da manutenção.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</section>
+
+<section class="section-servicos-diferenciados" style="background-image: linear-gradient(270deg, #4848C6 33.46%, rgba(72, 72, 198, 0) 100%),url(http://localhost/mitsui/wp-content/themes/mitsui/assets/images/fundo-servicos-diferenciados.png);">
+    <div class="container">
+
+        <div class="row justify-content-end">
+
+            <div class="text-white text-center text-md-start col-12 col-md-6 ">
+                <h2 class="fw300 pb-4 fw900">
+                    SERVIÇOS DIFERENCIADOS
+                </h2>
+                <p>A MRCLA pode oferecer serviços diferenciados à sua empresa, agregando todo nosso know-how adquirido através da implantação de vários projetos no setor ferroviário brasileiro e gestão de compra logística aplicadas com sucesso. <br><br>
+
+                    Nossa equipe está pronta para te auxiliar nos conceitos operacionais e gerenciais do transporte de carga ferroviário. Entre em contato conosco!
+                </p>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="section-nossa-experiencia container py-8">
+    <h2 class="fw300 pb-4 azul-escuro">NOSSA <fw900> EXPERIÊNCIA </fw900>
+    </h2>
+
+    <p>Nossa expertise na área foi adquirida ao longo de 17 anos de atuação junto a grandes players logísticos. Participamos de 90% da implantação no setor ferroviário, visando contribuir para infraestrutura e desenvolvimento social do Brasil. <br> <br>
+
+        Nossa equipe técnica, especializada e comprometida com seu negócio, prestará suporte para lidar com os complexos aspectos gerenciais na compra de material rodante para o transporte ferroviário. Pela nossa vasta experiência em projetos, podemos recomendar soluções de mobilidade integradas que levam a preços competitivos para sua empresa, independentemente do tamanho ou estrutura de suas operações.
+    </p>
+
+    <div class="row nesses-17">
+
+        <div class="d-flex flex-column flex-lg-row justify-content-center">
+            <div class="d-flex flex-column azul-roxo align-items-end gap-lg-5 col-lg-3">
+                <div class="d-flex align-items-center">
+                    <p>Realizou <strong>26</strong> projetos</p>
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/projetos.png" alt="">
+                </div>
+                <div class="d-flex align-items-center pe-lg-5">
+                    <p>Alugou <strong>9.712</strong> Vagões</p>
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/alugou-vagoes.png" alt="">
+                </div>
+                <div class="d-flex align-items-center">
+                    <p>Alugou <strong>21</strong> Locomotivas</p>
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/alugou-locomotivas.png" alt="">
+                </div>
+            </div>
+        
+            <div class="d-flex justify-content-center">
+                <img style="max-width: 382px;" src="<?php bloginfo('template_url'); ?>/assets/images/nesses-17-anos.svg" alt="">
+            </div>
+        
+            <div class="d-flex flex-column azul-roxo align-items-start gap-lg-5 col-lg-4 ">
+                <div class="d-flex align-items-center">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/construiu.png" alt="">
+                    <p>Construiu <strong>1</strong> terminal de cargas</p>
+                </div>
+                <div class="d-flex align-items-center ps-lg-5">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/contribuiu.png" alt="">
+                    <p>Contribuiu com <strong>14</strong> grandes players e diversos parceiros</p>
+                </div>
+                <div class="d-flex align-items-center">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/esta-presente.png" alt="">
+                    <p>Está presente em toda malha da Ferrovia Norte-Sul e Centro-Atlântica</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
